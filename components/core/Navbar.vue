c9d776e15bfc89580cf8da740dbb7fec2991f854
marquis-codes <abahmarquis@gmail.com>
2026-02-01 23:14:36 -0800

    updatd

--- components/core/Navbar.vue
+++ components/core/Navbar.vue
@@ -118,7 +118,7 @@
                         </a>
                       </li>
                       <li>
-                        <NuxtLink @click="isOpenSidebar = false" to="/waitlist" class="group flex items-center gap-x-3 rounded-full p-3 py-3 text-sm/6 font-semibold text-white bg-gradient-to-r from-emerald-600 to-emerald-700 hover:from-emerald-700 hover:to-emerald-800 transition-all duration-300">
+                        <NuxtLink @click="isOpenSidebar = false" to="/waitlist" class="group flex items-center gap-x-3 rounded-full p-3 py-3 text-sm/6 font-semibold text-white bg-gradient-to-r from-[#045940] to-[#067A4B] hover:from-[#067A4B] hover:to-[#045940] transition-all duration-300">
                           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"/>
                             <polyline points="10 17 15 12 10 7"/>
